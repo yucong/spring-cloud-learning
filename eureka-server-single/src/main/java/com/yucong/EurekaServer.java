b480f4e1c26f9b76a5fb2946faa2610b936bde6f
yucong <2241268051@qq.com>
2019-07-20 20:37:44 +0800

    eureka服务注册与发现

---
+++ eureka-server-single/src/main/java/com/yucong/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.yucong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 喻聪
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+}
---
+++ spring-cloud-02-eureka-server-cluster2/src/main/java/com/yucong/eureka/EurekaServerApplication.java
@@ -0,0 +1,33 @@
+package com.yucong.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+	
+	
+	/**
+     * 忽略此路径下的CSRF令牌
+     *
+     */
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            super.configure(http);
+        }
+    }    
+	
+}
