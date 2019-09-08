195156617e8796104c328fd9f6b2da93d2b4fdf3
yucong <2241268051@qq.com>
2019-09-08 10:38:18 +0800

    代码提交

--- spring-cloud-03-eureka-appservice-01/src/main/java/com/yucong/eureka/controller/TestApplicationServiceController.java
+++ spring-cloud-03-eureka-appservice-01/src/main/java/com/yucong/eureka/controller/TestApplicationServiceController.java
@@ -17,9 +17,19 @@ public class TestApplicationServiceController {
 	@ResponseBody
 	public List<Map<String, Object>> test(){
 		
+		// System.err.println("开始处理请求："  + Thread.currentThread().getName() );
+		
 		List<Map<String, Object>> result = new ArrayList<>();
 		
 		for(int i = 0; i < 3; i++){
+			// System.err.println("线程开始睡眠："  + Thread.currentThread().getName() );
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// System.err.println("线程睡眠结束："  + Thread.currentThread().getName() );
+			
 			Map<String, Object> data = new HashMap<>();
 			data.put("id", i+1);
 			data.put("name", "test name " + i);
@@ -27,6 +37,8 @@ public class TestApplicationServiceController {
 			
 			result.add(data);
 		}
+		
+		// System.err.println("请求处理结束：" + Thread.currentThread().getName() );
 		
 		return result;
 	}
